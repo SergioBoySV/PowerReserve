2de9284716fdacfceec0a9fa376d7d234636ac0a
SergioBoySV <darknight_777@hotmail.com>
2024-02-24 10:33:26 -0600

    Initial Commit

---
+++ PowerReserve/PowerReserveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerReserveApp.swift
+//  PowerReserve
+//
+//  Created by Sergio Villarreal on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PowerReserveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PowerReserve/SwiftUIView2.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView2.swift
+//  PowerReserve
+//
+//  Created by Sergio Villarreal on 2/24/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
